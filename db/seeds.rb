8c09fcacdfad7d95e8b5607b2451af7171809ebe
Sophearychiv <sopheary.chiv@gmail.com>
2019-06-24 14:59:46 -0700

    set up api data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-customer_data = JSON.parse(File.read('db/seeds/customers.json'))
+customer_data = JSON.parse(File.read("db/seeds/customers.json"))
 
 customer_data.each do |customer|
   Customer.create!(customer)
 end
 
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
-  movies = MovieWrapper.search(movie_data["title"])
-  ap "#{movie_data['title']} Added to the library!"
-  movies.first.inventory = movie_data['inventory']
-  movies.first.save unless movies.empty?
+JSON.parse(File.read("db/seeds/movies.json")).each do |movie_data|
+  begin
+    movies = MovieWrapper.search(movie_data["title"])
+    ap "#{movie_data["title"]} Added to the library!"
+    movies.first.inventory = movie_data["inventory"]
+    movies.first.save unless movies.empty?
+  rescue
+    retry
+  end
 end
