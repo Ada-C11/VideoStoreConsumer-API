9617f6fb9dffc1c1c417f16e9104a7cddae69d0e
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-06-25 14:13:49 -0700

    added create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,17 @@ class MoviesController < ApplicationController
       )
   end
 
+  def create
+
+    movie = Movie.new(title: params[:title], external_id: params[:external_id], release_date: params[:release_date], overview: params[:overview])
+
+    if movie.save
+      render json: movie.as_json(only: %i[title overview release_date id image_url external_id]), status: :ok
+    else
+      render json: { ok: false, message: movie.errors.messages }, status: :bad_request
+    end
+  end
+
   private
 
   def require_movie
@@ -29,4 +40,5 @@ class MoviesController < ApplicationController
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
   end
+
 end
