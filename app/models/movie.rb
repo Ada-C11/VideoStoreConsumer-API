4f6a71beadf4ef30852eba0dd6a00e22117b2d41
Shubha Rajan <shubharajan94@gmail.com>
2019-06-25 12:36:02 -0700

    added validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,10 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
+  validates :title, :inventory, presence: true
+
+  validates :inventory, numericality: { only_integer: true }
+
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
   end
