8e63ba670b903321d54946f159d0d5b76300feef
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-06-25 15:36:51 -0700

    FIXED PARAMS I SAID

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,11 +23,7 @@ class MoviesController < ApplicationController
 
   def create
 
-<<<<<<< HEAD
-    movie = Movie.new(title: params[:title], external_id: params[:external_id], release_date: params[:release_date], image_url: params[:image_url], overview: params[:overview])
-=======
     movie = Movie.new(title: params[:title], external_id: params[:external_id], release_date: params[:release_date], overview: params[:overview], image_url: params[:image_url])
->>>>>>> dc3cc1ee435c4f545068df97d25f00ba63f08b10
 
     if movie.save
       render json: movie.as_json(only: %i[title overview release_date id image_url external_id]), status: :ok
