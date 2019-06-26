2e7ddc4a6099e6d6a03ce7626af269e57fb7fcdf
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-26 14:48:10 -0700

    fixed image rendering problem in the create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
+  BASE_IMG_URL = "https://image.tmdb.org/t/p/"
+  DEFAULT_IMG_SIZE = "w185"
+
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
@@ -16,9 +19,9 @@ class MoviesController < ApplicationController
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
-        methods: [:available_inventory]
-        )
-      )
+        methods: [:available_inventory],
+      ),
+    )
   end
 
   def create
@@ -27,10 +30,13 @@ class MoviesController < ApplicationController
     # if it saves, return a 200 ok
     # if it doesnt, return a bad message
     movie = Movie.new(movie_params)
+    better_image = movie.image_url.gsub("https://image.tmdb.org/t/p/w185", "")
+    movie.image_url = better_image
+
     if movie.save
-      render json: { id: movie.id }, status: :ok
+      render json: {id: movie.id}, status: :ok
     else
-      render json: { errors: movie.errors.messages }, status: :bad_request
+      render json: {errors: movie.errors.messages}, status: :bad_request
     end
   end
 
@@ -39,7 +45,7 @@ class MoviesController < ApplicationController
   def require_movie
     @movie = Movie.find_by(title: params[:title])
     unless @movie
-      render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
+      render status: :not_found, json: {errors: {title: ["No movie with title #{params["title"]}"]}}
     end
   end
 
