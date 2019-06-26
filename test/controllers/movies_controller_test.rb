38a5f4c4d4196a0ebe406cf4660bc28a47833f80
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-26 13:10:56 -0700

    added testing for movie controller create action

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -76,5 +76,48 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   describe "create" do
+    let(:movie_params) {
+      {
+        title: "Thing",
+        overview: "Test",
+        release_date: "2019-01-01",
+        image_url: "www.dogs.jpg",
+        external_id: 3533,
+      }
+    }
+
+    it "should create a new movie given valid data" do
+      expect {
+        post movies_path(movie_params)
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+
+      expect(movie.title).must_equal movie_params[:title]
+      must_respond_with :success
+    end
+
+    # #  update validations if you want to use this test
+
+    # it "returns an error for invalid movie data" do
+    #   movie_params[:title] = nil
+
+    #   expect {
+    #     post movies_path(movie_params)
+    #   }.wont_change "Movie.count"
+
+    #   body = JSON.parse(response.body)
+
+    #   expect(body).must_be_kind_of Hash
+    #   expect(body).must_include "errors"
+    #   expect(body["errors"]).must_include "title"
+    #   expect(body["errors"]["title"]).must_equal ["can't be blank"]
+    #   must_respond_with :bad_request
+    # end
   end
 end
