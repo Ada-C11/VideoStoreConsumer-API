5b186778cee9fca1b623f9fed0c576a033cd6e17
Nidhi <nidhixpatel@gmail.com>
2019-06-27 10:13:28 -0700

    working on checkout

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,23 @@ class MoviesController < ApplicationController
       )
   end
 
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: { id: movie.id }, status: :ok
+    else
+      render json: { errors: movie.errors.messages }, status: :bad_request
+    end
+  end
+
+
   private
+
+  def movie_params
+    params.permit(:external_id, :image_url, :title, :overview, :release_date, :inventory)
+  end
 
   def require_movie
     @movie = Movie.find_by(title: params[:title])
